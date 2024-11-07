f7448608c214dcc8c1487431ebef4b864c9ec25e
Adam Cimarosti <cimarosti@gmail.com>
2024-11-07 16:55:51 +0000

    cargo fmt

--- src/hash/map.rs
+++ src/hash/map.rs
@@ -1,11 +1,11 @@
-use core::alloc::Allocator;
-use hashbrown::HashMap as InnerHashMap;
-pub use hashbrown::DefaultHashBuilder;
-pub use hashbrown::hash_map::{Keys, Values, ValuesMut};
 use crate::claim::Claim;
+use core::alloc::Allocator;
+pub use hashbrown::hash_map::{Keys, Values, ValuesMut};
+pub use hashbrown::DefaultHashBuilder;
+use hashbrown::HashMap as InnerHashMap;
 
 pub struct HashMap<K, V, A: Allocator, S = DefaultHashBuilder> {
-    inner: InnerHashMap<K, V, S, A>
+    inner: InnerHashMap<K, V, S, A>,
 }
 
 impl<K, V, A: Allocator + Claim> HashMap<K, V, A, DefaultHashBuilder> {
@@ -13,30 +13,30 @@ impl<K, V, A: Allocator + Claim> HashMap<K, V, A, DefaultHashBuilder> {
     pub fn new_in(alloc: A) -> Self {
         Self::with_hasher_in(DefaultHashBuilder::default(), alloc)
     }
-    
+
     #[inline]
     pub fn with_capacity_in(capacity: usize, alloc: A) -> Self {
         Self::with_capacity_and_hasher_in(capacity, DefaultHashBuilder::default(), alloc)
     }
 }
 
-impl<K, V, A: Allocator + Claim, S> HashMap<K, V, A, S> {    
+impl<K, V, A: Allocator + Claim, S> HashMap<K, V, A, S> {
     #[inline]
     pub fn allocator(&self) -> &A {
         self.inner.allocator()
     }
-    
+
     #[inline]
     pub fn with_hasher_in(hash_builder: S, alloc: A) -> Self {
         Self {
-            inner: InnerHashMap::with_hasher_in(hash_builder, alloc)
+            inner: InnerHashMap::with_hasher_in(hash_builder, alloc),
         }
     }
-    
+
     #[inline]
     pub fn with_capacity_and_hasher_in(capacity: usize, hash_builder: S, alloc: A) -> Self {
         Self {
-            inner: InnerHashMap::with_capacity_and_hasher_in(capacity, hash_builder, alloc)
+            inner: InnerHashMap::with_capacity_and_hasher_in(capacity, hash_builder, alloc),
         }
     }
 
@@ -44,7 +44,7 @@ impl<K, V, A: Allocator + Claim, S> HashMap<K, V, A, S> {
     pub fn hasher(&self) -> &S {
         &self.inner.hasher()
     }
-    
+
     #[inline]
     pub fn capacity(&self) -> usize {
         self.inner.capacity()
@@ -54,12 +54,12 @@ impl<K, V, A: Allocator + Claim, S> HashMap<K, V, A, S> {
     pub fn keys(&self) -> Keys<'_, K, V> {
         self.inner.keys()
     }
-    
+
     #[inline]
     pub fn values(&self) -> Values<'_, K, V> {
         self.inner.values()
     }
-    
+
     #[inline]
     pub fn values_mut(&mut self) -> ValuesMut<'_, K, V> {
         self.inner.values_mut()
