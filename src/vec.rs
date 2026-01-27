acb1ca428c5298436ad5044e515b60b7c0b62cc8
victorgao <kafka1991@163.com>
2026-01-28 04:19:42 +0800

    add `set_len` for `Vec<T, A: Allocator>` (#13)

--- src/vec.rs
+++ src/vec.rs
@@ -115,6 +115,16 @@ impl<T, A: Allocator> Vec<T, A> {
         self.inner.as_mut_ptr()
     }
 
+    /// # Safety
+    ///
+    /// - `new_len` must be less than or equal to [`capacity()`].
+    ///
+    /// [`capacity()`]: Self::capacity
+    #[inline]
+    pub unsafe fn set_len(&mut self, new_len: usize) {
+        self.inner.set_len(new_len);
+    }
+
     #[inline]
     pub fn clear(&mut self) {
         self.inner.clear();
